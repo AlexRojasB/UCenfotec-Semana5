72635b0dba608642c78c574ffc113be52a9e6d21
Alexander Rojas <alexrrojas.b@gmail.com>
2021-09-11 10:44:49 -0600

    Finish lab7

---
+++ lab7/lab7/lab7App.swift
@@ -0,0 +1,17 @@
+//
+//  lab7App.swift
+//  lab7
+//
+//  Created by Alexander Rojas Benavides on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct lab7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
